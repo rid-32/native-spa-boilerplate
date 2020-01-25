69979885dffeb5389ec1b9af57dcbda3d0e664f6
Denis Rostokin <denis.rostokin@osinit.com>
2020-01-25 18:55:26 +0300

    add webpack-dev-server port to the .env

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -17,7 +17,7 @@ module.exports = merge.smart(
     devServer: {
       historyApiFallback: true,
       host: '0.0.0.0',
-      port: 3000,
+      port: process.env.WEBPACK_DEVSERVER_PORT || 3000,
       hot: true,
       compress: true,
     },
